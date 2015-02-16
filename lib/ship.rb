79ea0b9393aa87b39e16406a84829b01e2fc25f7
Sean Haughton <seanphaughton@gmail.com>
2015-02-16 22:59:30 +0000

    Complete ship class and spec

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,18 +1,34 @@
 class Ship
 
-	attr_reader :size
+  attr_reader :length
 
-	def initialize(size = 1)
-		@size = size
-		@hit = false
-	end
+  def initialize(length = 3)
+    @length = length
+    @hits = 0
+  end
 
-	def hit?
-		@hit = true
-	end
+  def self.submarine
+    new 2
+  end
 
-	def status
-		@hit = true ? "sunk" : "missed"
-	end
+  def self.aircraft_carrier
+    new 5
+  end
+
+  def hit
+    @hits += 1
+  end
+
+  def sunk?
+    hits >= length
+  end
+
+  alias_method :size, :length
+
+private
+
+  def hits
+    @hits
+  end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,16 +1,47 @@
-require './lib/ship'
+require 'ship'
 
 describe Ship do
+  let(:ship){Ship.new}
 
-	let(:ship){Ship.new}
+  it "can be hit" do
+    ship.hit
+    expect(ship.send(:hits)).to eq 1
+  end
 
-	it "has a size" do
-		expect(ship.size).to eq 1
-	end
+  it "should know if a ship is not sunk" do
+    expect(ship).not_to be_sunk
+  end
 
-	it "should be able to have the status 'sunk'" do
-		ship.hit?
-		expect(ship.status).to eq "sunk"
-	end
+  it "should know if a ship is sunk when hit" do
+    ship = Ship.new(1)
+    ship.hit
+    expect(ship).to be_sunk
+  end
+
+context "Submarine" do
+
+  it "knows how big it is" do
+    expect(ship.length).to eq 3
+  end
+
+  it "knows that a submarine has a length of 2" do
+    submarine = Ship.submarine
+    expect(submarine.length).to eq 2
+  end
+
+  it "knows that its size is 2" do
+    submarine = Ship.submarine
+    expect(submarine.size).to eq 2
+  end
+
+end
+
+context "Aircraft carrier" do
+
+  it "knows that an aircraft carrier has a length of 5" do
+    expect(Ship.aircraft_carrier.length).to eq 5
+  end
+
+end
 
 end
