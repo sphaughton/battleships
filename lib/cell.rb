f51e5069f3dc31a838a5c5637b0495a55eefa777
Sean Haughton <seanphaughton@gmail.com>
2015-02-16 22:57:25 +0000

    Complete cell class and spec

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,17 @@
 class Cell
 
-	attr_accessor :value
+  attr_accessor :content
 
-	def initialize(value = '')
-		@value = value
-	end
+  def initialize
+    @hit = false
+  end
 
+  def shoot
+    content.hit
+    @hit = true
+  end
+
+  def hit?
+    @hit
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,26 +1,25 @@
 require 'cell'
 
-	describe Cell do
+  describe Cell do
 
-		let(:cell){Cell.new}
-		let(:ship){Ship.new}
-		
-		it "can be initialised with an empty value" do
-			expect(cell.value).to eq ''
-		end
+    let(:cell){Cell.new}
+    let(:ship){double :ship}
+    
+    it "has content" do
+      cell.content = ship
+      expect(cell.content).to eq ship
+    end
 
-		it "can be initialised with a value of 'missed'" do
-			cell = Cell.new("missed")
-			expect(cell.value).to eq "missed"
-		end
+    it "can be shot" do
+      cell.content = ship
+      allow(ship).to receive(:hit)
+      cell.shoot
+      expect(cell).to be_hit
+    end
 
-		it "can be initialised with a value of 'hit'" do
-			cell = Cell.new("hit")
-			expect(cell.value).to eq "hit"
-		end
-
-		it "can be initialised with a ship" do
-			cell = Cell.new("ship")
-			expect(cell.value).to eq "ship"
-		end
+    it "hits its content once shot" do
+      cell.content = ship
+      expect(ship).to receive(:hit)
+      cell.shoot
+    end
 end
